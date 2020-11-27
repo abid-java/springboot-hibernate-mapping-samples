e1011beb54fee121e9f81f6d5d6a23fe15430638
abmohamm <mohd.92java@gmail.com>
2020-11-27 17:26:09 -0500

    [US1234]:Skeleton project with Hibernate One To One Mapping

---
+++ springboot-hibernate-onetoone-mapping/src/main/java/com/app/samples/springboot/SpringbootHibernateOnetooneMappingApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateOnetooneMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateOnetooneMappingApplication.class, args);
+	}
+
+}
