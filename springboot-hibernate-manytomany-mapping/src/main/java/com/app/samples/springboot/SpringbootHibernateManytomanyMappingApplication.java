c67f189b6a1190a82440a9861c34269664d1e5aa
abmohamm <mohd.92java@gmail.com>
2020-11-27 20:34:59 -0500

    [US1234]:Created skeleton spring boot project for Many-to-Many mapping

---
+++ springboot-hibernate-manytomany-mapping/src/main/java/com/app/samples/springboot/SpringbootHibernateManytomanyMappingApplication.java
@@ -0,0 +1,22 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SpringbootHibernateManytomanyMappingApplication.
+ */
+@SpringBootApplication
+public class SpringbootHibernateManytomanyMappingApplication {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateManytomanyMappingApplication.class, args);
+	}
+
+}
